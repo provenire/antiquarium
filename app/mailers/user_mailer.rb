32a8a179354a46b132f82ac2233d654735e0253c
Miles Zimmerman <miles@maztek.io>
2015-01-19 08:16:27 -0800

    User mailers are set up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ApplicationMailer
 
   def activation_needed_email(user)
     @user = user
-    @url  = "https://data.antiquarium.io/users/activate?token=#{user.activation_token}"
+    @url  = "https://data.antiquarium.io/register?token=#{user.activation_token}"
 
-    mail to: user.email, subject: 'Antiquarium - Account Activation'
+    mail to: user.email, subject: 'You have been invited to join Antiquarium'
   end
 
   def activation_success_email(user)
