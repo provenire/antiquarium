cc67d46b28e35b11a77886fe7d6853d991f0143b
Miles Zimmerman <miles@maztek.io>
2015-02-07 01:46:53 -0800

    Citations/sources for artifacts, people, places

--- app/decorators/place_decorator.rb
+++ app/decorators/place_decorator.rb
@@ -33,4 +33,10 @@ class PlaceDecorator < ObjectDecorator
     model.photos.first && model.photos.first.image.show_half.url || 'no_photo/500.png'
   end
 
+  def citations_list
+    citeproc = CiteProc::Processor.new(style: 'chicago-fullnote-bibliography', format: 'text')
+    model.citations.map(&:cite).each{|cite| citeproc << cite }
+    citeproc.bibliography.to_a
+  end
+
 end
